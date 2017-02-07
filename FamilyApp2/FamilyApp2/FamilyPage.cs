037e553dbae38b4716a22011ba4daa81a7ca9e27
KongPKhang <KhangKongP@users.noreply.github.com>
2017-02-06 21:37:14 -0600

    Finish

--- FamilyApp2/FamilyApp2/FamilyPage.cs
+++ FamilyApp2/FamilyApp2/FamilyPage.cs
@@ -16,18 +16,17 @@ namespace FamilyApp2
             var stackNew = new StackLayout
             {
 
-                HorizontalOptions = LayoutOptions.Center,
                 BackgroundColor = Color.Black,
                 Spacing = 3.0,
-                Orientation = StackOrientation.Horizontal,
+                Orientation = StackOrientation.Vertical,
                 Children =
                 {
                     
-                    new Label {Text = "Mee - my mother", TextColor = Color.Purple,  FontAttributes = FontAttributes.Bold, FontSize = 20, HorizontalOptions = LayoutOptions.CenterAndExpand, BackgroundColor = Color.Green  },
-                    new Label {Text = "Nha - my father", TextColor = Color.Yellow, FontSize = 20, FontAttributes = FontAttributes.Bold },
-                    new Label {Text = "Kong - me", TextColor = Color.Orange, FontSize = 21, FontAttributes = FontAttributes.Bold},
-                    new Label {Text = "Zachariah - my son", TextColor = Color.Blue, FontSize = 22, FontAttributes = FontAttributes.Bold },
-                    new Label {Text = "Nouzong - my wife", TextColor = Color.Pink, FontSize = 23, FontAttributes = FontAttributes.Bold }
+                    new Label {Text = "Mee - my mother ", TextColor = Color.Purple,  FontAttributes = FontAttributes.Bold, FontSize = 20, VerticalOptions = LayoutOptions.StartAndExpand, BackgroundColor = Color.Green  },
+                    new Label {Text = "Nha - my father", TextColor = Color.Yellow, FontSize = 30, FontAttributes = FontAttributes.Bold },
+                    new Label {Text = "Kong - me ", TextColor = Color.Orange, FontSize = 41, FontAttributes = FontAttributes.Bold},
+                    new Label {Text = "Zachariah - my son", TextColor = Color.Blue, FontSize = 42, FontAttributes = FontAttributes.Bold },
+                    new Label {Text = "Nouzong - my wife", TextColor = Color.Pink, FontSize = 43, FontAttributes = FontAttributes.Bold }
 
                 }
             
@@ -36,7 +35,7 @@ namespace FamilyApp2
 
             Content = new ScrollView
             {
-                Orientation = ScrollOrientation.Horizontal,
+                Orientation = ScrollOrientation.Vertical,
                 Content = stackNew
                 
             };
