20837433cda38390690524f20cfb4ff129d2f0d7
KongPKhang <KhangKongP@users.noreply.github.com>
2017-02-06 20:47:11 -0600

    Made Scrolling changes

--- FamilyApp2/FamilyApp2/FamilyPage.cs
+++ FamilyApp2/FamilyApp2/FamilyPage.cs
@@ -7,27 +7,38 @@ using Xamarin.Forms;
 
 namespace FamilyApp2
 {
-    class FamilyPage : ContentPage  
+    class FamilyPage : ContentPage
     {
 
         public FamilyPage()
         {
-            Content = new StackLayout
+            var stackNew = new StackLayout
             {
 
-                BackgroundColor = Color.Pink,
+                BackgroundColor = Color.Black,
                 Spacing = 3.0,
-                
+
+                Orientation = StackOrientation.Horizontal,
                 Children =
                 {
-                    new Label {Text = "Mee - my mother", TextColor = Color.Purple, FontAttributes = FontAttributes.Bold },
-                    new Label {Text = "Nha - my father", TextColor = Color.Yellow },
-                    new Label {Text = "Kong - me", TextColor = Color.Orange},
-                    new Label {Text = "Zachariah - my son", TextColor = Color.Blue },
-                    new Label {Text = "Nouzong - my wife", TextColor = Color.Pink }
+
+                    new Label {Text = "Mee - my mother", TextColor = Color.Purple,  FontAttributes = FontAttributes.Bold, FontSize = 20 },
+                    new Label {Text = "Nha - my father", TextColor = Color.Yellow, FontSize = 20, FontAttributes = FontAttributes.Bold },
+                    new Label {Text = "Kong - me", TextColor = Color.Orange, FontSize = 21, FontAttributes = FontAttributes.Bold},
+                    new Label {Text = "Zachariah - my son", TextColor = Color.Blue, FontSize = 22, FontAttributes = FontAttributes.Bold },
+                    new Label {Text = "Nouzong - my wife", TextColor = Color.Pink, FontSize = 23, FontAttributes = FontAttributes.Bold }
+
                 }
-               
+            
+
             };
+
+            Content = new ScrollView
+            {
+                Orientation = ScrollOrientation.Horizontal,
+                Content = stackNew
+            };
+
 
         }
 
