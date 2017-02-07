9725120c25740bd190dd3dc93c3123e41af86ce8
htcstudent <htcstudent@EH255WD18>
2017-02-06 20:00:52 -0600

    Add project files.

---
+++ FamilyApp2/FamilyApp2/FamilyPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FamilyApp2
+{
+    class FamilyPage : ContentPage  
+    {
+
+        public FamilyPage()
+        {
+            Content = new Label
+            {
+
+                BackgroundColor = Color.Pink,
+
+                FormattedText = new FormattedString
+                {
+                    Spans =
+                    {
+                        new Span
+                        {
+                        Text = "Nha Vang Khang  Daddy " + Environment.NewLine,
+                        FontAttributes = FontAttributes.Bold,
+                        BackgroundColor = Color.Yellow
+
+                        },
+                        new Span
+                        {
+                            Text = "Mee Y Khang  Mommy" + Environment.NewLine,
+                            FontAttributes = FontAttributes.Bold,
+                            BackgroundColor = Color.Blue
+                        },
+                        new Span
+                        {
+                            Text = "Kong P Khang ME!" + Environment.NewLine,
+                            FontAttributes = FontAttributes.Bold,
+                            BackgroundColor = Color.Red
+                        }
+                    }
+                }
+            };
+
+        }
+
+
+    }
+}
